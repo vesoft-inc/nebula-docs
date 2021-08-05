e0964defb2dbc1f67e5e7498c926a0d259d9c071
jerry.liang <8124783+nianiaJR@users.noreply.github.com>
2021-08-05 03:55:10 +0000

    Deployed a3b33476f to 2.0.1 with MkDocs 1.1 and mike 1.0.1

--- 2.0.1/js/search.js
+++ 2.0.1/js/search.js
@@ -1,18 +1,4 @@
 (function(){
-    $("img").click(function change(){
-        var $this = $(this)
-        $(this).toggleClass("max");
-        $("#dialog-bg").toggleClass("dialog-bg");
-    });
-    $("#dialog-bg").on("click",function () {
-        $("#dialog-bg").toggleClass("dialog-bg");
-        $("img").each(function(){
-            if($(this).hasClass("max")){
-                $(this).toggleClass("max");
-            }
-        })
-    });
-    
     const Search={
         init:function(){
             this.attachEvent();
