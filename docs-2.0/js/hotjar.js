291f7474a8db1932d7bc61085396fc67aeae203f
randomJoe211 <69501902+randomJoe211@users.noreply.github.com>
2023-04-18 17:15:42 +0800

    Update hotjar.js

--- docs-2.0/js/hotjar.js
+++ docs-2.0/js/hotjar.js
@@ -1,11 +1,10 @@
-<!-- Hotjar Tracking Code for https://docs.nebula-graph.io/ -->
-<script>
-    (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:2875713,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-</script>
+// Hotjar Tracking Code for https://docs.nebula-graph.io/
+
+(function (h, o, t, j, a, r) {
+    h.hj = h.hj || function () { (h.hj.q = h.hj.q || []).push(arguments) };
+    h._hjSettings = { hjid: 2875713, hjsv: 6 };
+    a = o.getElementsByTagName('head')[0];
+    r = o.createElement('script'); r.async = 1;
+    r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+    a.appendChild(r);
+})(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
